adae939c1462fd59e79bfe8f8684bb1aa5380107
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2024-12-13 23:56:23 +0900

    ランダマイズ

--- game.gd
+++ game.gd
@@ -24,13 +24,17 @@ func _on_enemy_timer_timeout() -> void:
 	
 	var location = $LeftSpawnLocation if randi() % 2 == 0 else $RightSpawnLocation 
 	enemy.position = location.position
+	enemy.speed = 200 if randi() % 2 else 100
 	
 	enemy.turn($Player.global_position.x < enemy.global_position.x)
 	
 	add_child(enemy)
+	
+	$EnemyTimer.start(randf_range(.5,2))
 
 func _on_player_hit_by_enemy() -> void:
 	game_over()
 
 func _on_player_hit(enemy: Node2D) -> void:
+	$HitSound.play()
 	enemy.death()
